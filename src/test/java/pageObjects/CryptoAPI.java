8c8600f3c6db6f1cd2ad321016076d2bfeb8ef1c
git-wasim <wiqbal1007@gmail.com>
2023-01-27 05:37:05 +0530

    api common auth method added

--- src/test/java/pageObjects/CryptoAPI.java
+++ src/test/java/pageObjects/CryptoAPI.java
@@ -2,52 +2,46 @@ package pageObjects;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class CryptoAPI {
-	
+
 	public static String GBP;
 	public static String DOGGE;
-	
-	public void priceGTQtoGBP(String apiKey, String apiValue)
-	{
-		RestAssured.baseURI = "https://pro-api.coinmarketcap.com";
-		
-		Response res = RestAssured.given()
-				.header(apiKey,apiValue)
+
+	public String apiDomain = "https://pro-api.coinmarketcap.com";
+	public String apiEndpoint = "/v2/tools/price-conversion";
+
+	public RequestSpecification apiAuth(String apiKey, String apiValue) {
+		RestAssured.baseURI = apiDomain;
+		RequestSpecification reqspec = RestAssured.given().header(apiKey, apiValue);
+		return reqspec;
+	}
+
+	public void priceGTQtoGBP(String apiKey, String apiValue) {
+		Response res = apiAuth(apiKey, apiValue)
 				.queryParam("amount", "10000000")
 				.queryParam("convert", "GBP")
 				.queryParam("symbol", "GTQ")
-				.when()
-				.get("/v2/tools/price-conversion");
-		
-		GBP = res.then()
-				 .extract()
-				 .path("data[0].quote.GBP.price").toString();
-				 
-		
+				.when().get(apiEndpoint);
+
+		GBP = res.then().extract().path("data[0].quote.GBP.price").toString();
+
 		System.out.println(GBP);
-		
+
 	}
-	
-	public void priceGBPtoDOGGE(String apiKey, String apiValue)
-	{
-		RestAssured.baseURI = "https://pro-api.coinmarketcap.com";
-		
-		Response res = RestAssured.given()
-				.header(apiKey,apiValue)
+
+	public void priceGBPtoDOGGE(String apiKey, String apiValue) {
+		Response res = apiAuth(apiKey, apiValue)
 				.queryParam("amount", GBP)
 				.queryParam("convert", "DOGE")
 				.queryParam("symbol", "GBP")
-				.when()
-				.get("/v2/tools/price-conversion");
-		
-		DOGGE = res.then()
-				.extract()
-				.path("data[0].quote.DOGE.price").toString();
-		
+				.when().get(apiEndpoint);
+
+		DOGGE = res.then().extract().path("data[0].quote.DOGE.price").toString();
+
 		System.out.println(DOGGE);
-		
+
 	}
-	
 
 }
